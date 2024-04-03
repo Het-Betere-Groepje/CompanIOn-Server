a68d79369a8cad8a63b05dea81a2fe0bb8e4cef7
Kian Bruyenberg <kbruyenberg@gmail.com>
2024-04-03 13:18:34 +0200

    Update consent.js

--- node-server/data/v2/consent.js
+++ node-server/data/v2/consent.js
@@ -1,9 +1,11 @@
 import { con } from '../../database.js';
 
 const getConsentLevels = async () => {
-    const [results] = await con.query('')
+    const [results] = await con.query('SELECT id, level, beschrijving FROM Consent')
     return results;
 }
 export default {
     getConsentLevels
 }
+
+//localhost:5000/v2/consent/
